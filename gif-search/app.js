8aa79e5236d46eda8ee2239081264e8d27e499c5
altayozyavas <altayozyavas@gmail.com>
2022-06-17 19:23:42 -0500

    added search form

--- gif-search/app.js
+++ gif-search/app.js
@@ -20,18 +20,22 @@ const hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 
 // Routes
-app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('hello-gif.handlebars', { gifUrl })
-  });
+// app.get('/', (req, res) => {
+//     // set the url of the gif
+//     const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+//     // render the hello-gif view, passing the gifUrl into the view to be displayed
+//     res.render('hello-gif.handlebars', { gifUrl })
+//   });
 
-app.get('/greetings/:name', (req, res) => {
-    // grab the name from the path provided
-    const name = req.params.name;
-    // render the greetings view, passing along the name
-    res.render('greetings.handlebars', { name });
+// app.get('/greetings/:name', (req, res) => {
+//     // grab the name from the path provided
+//     const name = req.params.name;
+//     // render the greetings view, passing along the name
+//     res.render('greetings.handlebars', { name });
+// })
+app.get('/', (req, res) => {
+    console.log(req.query) // => "{ term: hey" }
+    res.render('home.handlebars')
 })
 
 // Start Server
