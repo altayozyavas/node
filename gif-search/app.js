ab465cf3ed81c98c55024a36e502199ff81a69e0
altayozyavas <altayozyavas@gmail.com>
2022-06-17 19:16:51 -0500

    adding middleware

--- gif-search/app.js
+++ gif-search/app.js
@@ -5,10 +5,33 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs = require("express-handlebars");
+
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+
+// app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" })); 
+// app.set("view engine", "hbs");
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+});
+
+app.engine('handlebars', hbs.engine);
 
 // Routes
-app.get("/", (res, req) => {
-    req.send("Hello Moto!");
+app.get('/', (req, res) => {
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif.handlebars', { gifUrl })
+  });
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings.handlebars', { name });
 })
 
 // Start Server
